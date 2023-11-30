47744b722e004ac8c551a15915e8fa5a0cabd0c8
Brady Barnett <23532649+bradyabarnett@users.noreply.github.com>
2023-11-30 15:43:43 -0800

    Candyce Code

---
+++ I2.py
@@ -0,0 +1,19 @@
+
+from microbit import *
+display.off()
+
+while True:
+    x = pin1.read_analog()
+    if x < 700:
+        pin4.write_digital(1)
+        pin6.write_digital(1)
+        pin7.write_digital(1)
+        pin8.write_digital(1)
+        pin9.write_digital(1)
+    else:
+        pin4.write_digital(0)
+        pin6.write_digital(0)
+        pin7.write_digital(0)
+        pin8.write_digital(0)
+        pin9.write_digital(0)
+
